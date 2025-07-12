d6bd28007799b9f9bd8fc6a0c5144b0bd2250fff
Joel John Thomas <joeljt2468@gmail.com>
2025-07-11 21:49:10 -0500

    Update ProducerConsumerDemo.java

--- ProducerConsumerDemo.java
+++ ProducerConsumerDemo.java
@@ -1,13 +1,3 @@
-// ***************************************************************************
-// 
-//   Joel John Thomas 
-//   Z2004783
-//   CSCI 470 - PE1 
-// 
-//   I certify that this is my own work and where appropriate an extension 
-//   of the starter code provided for the assignment. 
-// ***************************************************************************
-
 import java.util.LinkedList;
 
 // Shared buffer with synchronized produce and consume methods
